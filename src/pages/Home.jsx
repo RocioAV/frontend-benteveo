59ac7d85963e3b5767dfe7fc2e897901a7cdb9a4
RocioAV <rocio.velazquez.0212@gmail.com>
2026-07-10 11:56:20 -0300

    Remove hero image from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 
-import heroImg from '../assets/hero.png'
 
 function Home() {
   return (
     <div className="home">
       <h1>Bienvenido a Benteveo</h1>
       <p>Tu plataforma de streaming de películas y series favorita.</p>
-      <img src={heroImg} alt="Hero" className="hero-image" />
     </div>
   )
 }
