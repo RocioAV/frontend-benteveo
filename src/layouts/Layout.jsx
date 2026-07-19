12d685428c86d23299ed1beda7fa8bb2850ba7d4
RocioAV <rocio.velazquez.0212@gmail.com>
2026-07-19 02:25:24 -0300

    Add Footer component and styles for improved site layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,23 +1,18 @@
 // src/layouts/Layout.jsx
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header'; // Tu componente de Header
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Layout() {
   return (
     <div className="app-container">
-      {/* El Header se queda fijo arriba de todo */}
       <Header />
 
-      {/* El main es el contenedor del contenido dinámico */}
       <main className="main-content">
-        {/* Aquí adentro React Router va a inyectar la página de la URL actual */}
         <Outlet /> 
       </main>
 
-      {/* El Footer se queda fijo abajo de todo */}
-      <footer className="app-footer">
-        <p>&copy; 2026 - Proyecto Benteveo. Todos los derechos reservados.</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
