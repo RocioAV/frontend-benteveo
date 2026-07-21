4a3918c3b08769084a03ca3cf7d47c75bda29078
Kevin Davalos <kevin.davalos.01@gmail.com>
2026-07-21 00:30:32 -0300

    Crear pagina de detalle de producto y ruta /detalle/:id

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Reservation from './pages/Reservation.jsx'
+import DetalleProducto from './pages/DetalleProducto.jsx'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Layout from './layouts/Layout.jsx'
@@ -13,6 +14,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/explorar" element={<PageCatalogo />} />
+        <Route path="/detalle/:id" element={<DetalleProducto />} />
         <Route path="/reservation/:id" element={<Reservation />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registro />} />
