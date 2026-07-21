3f948b596faf4ea00ae000bdff6325dda8db5463
Kevin Davalos <kevin.davalos.01@gmail.com>
2026-07-21 01:01:02 -0300

    scrool to top al cambiar de ruta, se implementa para corregir que arranque desde arriba del todo

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,21 @@
 // src/layouts/Layout.jsx
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function Layout() {
   return (
     <div className="app-container">
+      <ScrollToTop />
       <Header />
 
       <main className="main-content">
